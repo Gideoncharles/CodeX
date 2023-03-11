0e7d7983a6bebbf2c3aaeccba013f66a5e7b65e4
Gideoncharles <maogagideon1083@gmail.com>
2023-03-11 23:32:48 +0300

    update the server url

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from the server -> bot's respponse
 
-  const respponse = await fetch('http://localhost:5000', {
+  const respponse = await fetch('https://codex-jusy.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
